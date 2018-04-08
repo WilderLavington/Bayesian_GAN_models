57792a48e34d005baa3899bcd41c205d9578fea0
Felix Jimenez <felixjimenez@Felixs-MacBook-Pro.local>
2018-04-07 22:46:07 -0600

    adding the gan code for mnsit from the yihui github repo

---
+++ mnist_gan_yihui_00/main.py
@@ -0,0 +1,17 @@
+from mnist_gan_yihui_00.load import *
+
+
+
+def main():
+    print('Running . . . ')
+
+    trX, teX, trY, teY = mnist()
+
+
+
+
+    print('end')
+
+
+if __name__ == "__main__":
+    main()
